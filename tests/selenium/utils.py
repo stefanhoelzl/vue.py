b2ae5a0599c272540758ae8337ff9ffd508d12d6
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2018-07-07 12:16:59 +0200

    [tests] analyze console logs for error messages

--- tests/selenium/utils.py
+++ tests/selenium/utils.py
@@ -1,4 +1,5 @@
 import inspect
+import re
 from pathlib import Path
 
 import pytest
@@ -7,6 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import *
 
 URL = "http://localhost:8000/tests/selenium/_html/{}.html"
@@ -14,23 +16,58 @@ DEFAULT_TIMEOUT = 1
 
 
 @pytest.fixture(scope="session")
-def selenium():
+def selenium_():
     Path("_html").mkdir(exist_ok=True)
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
-    driver = webdriver.Chrome("./chromedriver", chrome_options=options)
+
+    desired = DesiredCapabilities.CHROME
+    desired['loggingPrefs'] = {"browser": "ALL"}
+
+    driver = webdriver.Chrome("./chromedriver",
+                              chrome_options=options,
+                              desired_capabilities=desired)
     yield driver
     driver.close()
 
 
-def prepare(selenium, app, html=""):
+@pytest.fixture
+def selenium(selenium_):
+    logs = []
+    def get_logs_and_save(only_new=True):
+        new_logs = selenium_.get_log("browser")
+        logs.extend(new_logs)
+        return new_logs if only_new else logs
+    selenium_.get_logs = get_logs_and_save
+    yield selenium_
+    analyze_logs(selenium_.get_logs(only_new=False))
+
+
+def analyze_logs(logs):
+    errors = []
+    exceptions = [
+        r"[^ ]+ 7692 Synchronous XMLHttpRequest on the main thread is deprecated because of its detrimental effects to the end user's experience. For more help, check https://xhr.spec.whatwg.org/.",
+        r"[^ ]+ {}".format(re.escape("7706 Failed to load resource: the server responded with a status of 404 (File not found)")),
+    ]
+    for log in logs:
+        if log['level'] != "INFO":
+            for exception in exceptions:
+                if re.match(exception, log["message"]):
+                    break
+            else:
+                errors.append(log)
+    if errors:
+        raise Exception("\n".join(str(error) for error in errors))
+
+
+
+def prepare(selenium, app):
     name = app.__name__
     code = inspect.getsource(app)
     code = "\n".join(l[4:] for l in code.split("\n"))
     code += "  app = {}('#app')".format(name)
     template = Path("index.html").read_text()
     template = template.replace("CODE", code)
-    template = template.replace("HTML", html)
     Path("_html", "{}.html".format(name)).write_text(template)
     selenium.get(URL.format(name))
 
