01dc2b9bb45bd6acd3472cb9f9e7ce99cb2345e8
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2018-07-15 15:48:12 +0200

    [docs] deploy _html to ci-results

---
+++ deploy.py
@@ -0,0 +1,12 @@
+import os
+
+from cico import TravisCI, GitHub
+from cico.results import Directory
+
+TravisCI(
+    repo=GitHub('stefanhoelzl', 'ci-results', os.environ.get("GH_TOKEN", "")),
+    branch="vue.py",
+    results=[
+        Directory("tests/selenium/_html", rename="html"),
+    ]
+).commit()
