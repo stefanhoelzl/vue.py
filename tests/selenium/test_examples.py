49c67523be8ee0d3967f5667b06ee81f520e0856
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2018-07-17 22:12:20 +0200

    [docs] added tests for TreeView example

--- tests/selenium/test_examples.py
+++ tests/selenium/test_examples.py
@@ -1,5 +1,7 @@
 import time
 
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 def test_markdown_editor(selenium):
     with selenium.example():
@@ -21,13 +23,6 @@ def test_markdown_editor(selenium):
         selenium.element_has_text("sub-title", "Sub Title")
 
 
-def test_github_commits(selenium):
-    with selenium.example():
-        assert selenium.element_with_tag_name_present("ul")
-        time.sleep(1)
-        assert 10 == len(selenium.driver.find_elements_by_tag_name("li"))
-
-
 def test_grid_component(selenium):
     with selenium.example():
         time.sleep(0.5)
@@ -41,3 +36,20 @@ def test_grid_component(selenium):
         assert "8000" == rows[1].text
         assert "Jackie Chan" == rows[2].text
         assert "7000" == rows[3].text
+
+
+def test_tree_view(selenium):
+    with selenium.example():
+        time.sleep(0.5)
+        lis = selenium.find_elements_by_tag_name("li")
+        lis[0].click()
+        lis[3].click()
+        ActionChains(selenium.driver).double_click(lis[8]).perform()
+        assert selenium.find_elements_by_tag_name("li")[9].text == "new stuff"
+
+
+def test_github_commits(selenium):
+    with selenium.example():
+        assert selenium.element_with_tag_name_present("ul")
+        time.sleep(1)
+        assert 10 == len(selenium.driver.find_elements_by_tag_name("li"))
