3f4b7ae5db7c445acade2e5421cc58d508eab755
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2018-07-20 19:46:58 +0200

    [examples] separate app and components

--- examples/element_ui/app.py
+++ examples/element_ui/app.py
@@ -1,99 +1,18 @@
-from vue import VueComponent, computed
-
-
-class NavigationItem(VueComponent):
-    item: dict
-    template = """
-    <el-menu-item-group v-if="is_group_header">
-        <span slot="title">{{ item.group }}</span>
-    </el-menu-item-group>
-    <component v-else
-        @click="click()"
-        :is="item_tag" 
-        :disabled="item.disabled"
-        :index="item.id">
-        <template v-if="is_submenu">
-            <template slot="title">
-                <i :class="item.icon"></i>
-                <span slot="title">{{ item.title }}</span>
-            </template>
-            <navigation-item 
-                v-for="sub_item in item.children"
-                :key="sub_item.id"
-                :item="sub_item"
-                >
-            </navigation-item>
-        </template>
-        <template v-else>
-            <i :class="item.icon"></i>
-            {{ item.title }}
-        </template>
-    </component>
-    """
-
-    @computed
-    def item_tag(self):
-        if self.is_submenu:
-            return "el-submenu"
-        return "el-menu-item"
-
-    @computed
-    def is_menu_item(self):
-        return not self.is_group_header and not self.is_submenu
-
-    @computed
-    def is_group_header(self):
-        return "group" in self.item
-
-    @computed
-    def is_submenu(self):
-        return "children" in self.item
-
-    def click(self):
-        self.notify.info({
-            "title": "Navigation",
-            "message": self.item.get("title", "NO TITLE")
-        })
-
-
-NavigationItem.register()
-
-
-class NavigationMenu(VueComponent):
-    content: list
-    template = """
-    <div>
-        <el-menu
-            @select="select"
-            class="navigation-menu">
-            <navigation-item 
-                v-for="item in content"
-                :key="item.id"
-                :item="item"
-                >
-            </navigation-item>
-        </el-menu>
-    </div>
-    """
-
-    def select(self, index, path, *args):
-        self.emit("select", index, path)
-
-
-NavigationMenu.register()
+from vue import VueComponent
+from vue.wrapper import Object
 
 
 class App(VueComponent):
     template = """
     <div>
         <navigation-menu 
-            @select="selected"
+            @click="clicked"
             :content="navigation_menu">
         </navigation-menu>
     </div>
     """
 
-    navigation_menu = content = [
+    navigation_menu = [
         {"id": "One", "title": "Navigation One", "icon": "el-icon-location",
          "children": [
              {"group": "Group One"},
@@ -112,8 +31,12 @@ class App(VueComponent):
         {"id": "Four", "title": "Navigation Four", "icon": "el-icon-setting"},
     ]
 
-    def selected(self, index, path):
-        print(index, path)
+    def clicked(self, item):
+        print(item)
+        self.notify.info({
+            "title": "Navigation",
+            "message": item.get("title", "NO TITLE")
+        })
 
 
 App("#app")
---
+++ examples/element_ui/components/navigation.py
@@ -0,0 +1,74 @@
+from vue import VueComponent, computed
+
+
+class NavigationItem(VueComponent):
+    item: dict
+    template = """
+    <el-menu-item-group v-if="is_group_header">
+        <span slot="title">{{ item.group }}</span>
+    </el-menu-item-group>
+    <component v-else
+        @click="$emit('click', item)"
+        :is="item_tag" 
+        :disabled="item.disabled"
+        :index="item.id">
+        <template v-if="is_submenu">
+            <template slot="title">
+                <i :class="item.icon"></i>
+                <span slot="title">{{ item.title }}</span>
+            </template>
+            <navigation-item 
+                v-for="sub_item in item.children"
+                :key="sub_item.id"
+                :item="sub_item"
+                >
+            </navigation-item>
+        </template>
+        <template v-else>
+            <i :class="item.icon"></i>
+            {{ item.title }}
+        </template>
+    </component>
+    """
+
+    @computed
+    def item_tag(self):
+        if self.is_submenu:
+            return "el-submenu"
+        return "el-menu-item"
+
+    @computed
+    def is_menu_item(self):
+        return not self.is_group_header and not self.is_submenu
+
+    @computed
+    def is_group_header(self):
+        return "group" in self.item
+
+    @computed
+    def is_submenu(self):
+        return "children" in self.item
+
+
+NavigationItem.register()
+
+
+class NavigationMenu(VueComponent):
+    content: list
+    template = """
+    <div>
+        <el-menu
+            class="navigation-menu">
+            <navigation-item 
+                @click="$emit('click', $event)"
+                v-for="item in content"
+                :key="item.id"
+                :item="item"
+                >
+            </navigation-item>
+        </el-menu>
+    </div>
+    """
+
+
+NavigationMenu.register()
