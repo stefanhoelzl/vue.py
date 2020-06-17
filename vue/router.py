bbd1bfcae0bd738fcab1c16e1fd568d30ed70e90
Vitor Daniel <vitor036daniel@gmail.com>
2020-06-17 17:16:27 -0300

    Add suport for anhother router lib

--- vue/router.py
+++ vue/router.py
@@ -11,8 +11,9 @@ class VueRouter(Wrapper):
     def init_dict(cls):
         return VueRouterFactory.get_item(cls)
 
-    def __new__(cls):
-        return window.VueRouter.new(cls.init_dict())
+    def __new__(cls, VueRouterObject=window.VueRouter):
+        window.Vue.use(VueRouterObject)
+        return VueRouterObject.new(cls.init_dict())
 
 
 class VueRoute:
