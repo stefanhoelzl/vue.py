14bd4cdf76ad64c37304f1c35efb0eb2feec0c7d
Vitor Daniel <vitor036daniel@gmail.com>
2020-06-18 16:15:25 -0300

    [internal] remove unnecessary code

--- vue/router.py
+++ vue/router.py
@@ -2,24 +2,18 @@ from browser import window
 from .factory import Wrapper, VueRouterFactory
 
 
-def init_dict(component):
-    return component.init_dict()
-
-
 class VueRouter(Wrapper):
     @classmethod
     def init_dict(cls):
         return VueRouterFactory.get_item(cls)
 
     def __new__(cls, VueRouterObject=window.VueRouter):
-        window.Vue.use(VueRouterObject)
         return VueRouterObject.new(cls.init_dict())
 
 
 class VueRoute:
     def __new__(cls, path, component=None, components=None, **kwargs):
         route = {"path": path, **kwargs}
-        route.update(kwargs)
         
         if component is not None:
             route["component"] = component.init_dict()
