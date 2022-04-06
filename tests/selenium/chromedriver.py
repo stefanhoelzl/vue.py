eec3879bae73f7043a0db05d00fc336a6814b12d
Stefan Hoelzl <1478183+stefanhoelzl@users.noreply.github.com>
2022-04-06 16:12:18 +0000

    [tooling] auto determine chromedriver version

--- tests/selenium/chromedriver.py
+++ tests/selenium/chromedriver.py
@@ -2,27 +2,29 @@ import re
 from subprocess import run
 
 import pyderman
+import requests
 
-# https://chromedriver.chromium.org/downloads
-Versions = {
-    "88": "88.0.4324.96",
-    "83": "83.0.4103.39",
-    "96": "96.0.4664.45",
-    "98": "98.0.4758.102",
-}
+LatestReleaseUrl = (
+    "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{version}"
+)
 
-chrome_version = (
+chrome_version_output = (
     run(["google-chrome", "--version"], capture_output=True)
     .stdout.decode("utf-8")
     .strip()
 )
-print(chrome_version)
-major = re.search("Google Chrome (\d+)", chrome_version).group(1)
+
+print(chrome_version_output)
+chrome_major_version = re.search("Google Chrome (\d+)", chrome_version_output).group(1)
+chromedriver_version = requests.get(
+    LatestReleaseUrl.format(version=chrome_major_version)
+).text.strip()
+print(f"Chromedriver Version {chromedriver_version}")
 
 pyderman.install(
     browser=pyderman.chrome,
     file_directory="tests/selenium",
     filename="chromedriver",
     overwrite=True,
-    version=Versions[major],
+    version=chromedriver_version,
 )
